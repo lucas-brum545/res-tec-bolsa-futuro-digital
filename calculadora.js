50294eb3e3fa90a3331fe64126967de58f658cd0
Lucas Brum <lucas@example.com>
2026-07-10 16:43:59 -0300

    codigos e pdf aula 1007 e exercicios

--- calculadora.js
+++ calculadora.js
@@ -1,25 +1,33 @@
 import leitor from 'readline-sync'
+import './funcoes.js'
+import { calculadora, lerNumero } from './funcoes.js'
 
-let v1 = leitor.question("Digite o primeiro valor: ")
-let v2 = leitor.question("Digite o segundo valor: ")
-let operacao = leitor.question("Entre com a operacao desejada (+, -, *, /): ")
+// let v1 = leitor.question("Digite o primeiro valor: ")
+// let v2 = leitor.question("Digite o segundo valor: ")
+// let operacao = leitor.question("Entre com a operacao desejada (+, -, *, /): ")
 
-v1 = Number(v1)
-v2 = Number(v2)
+// v1 = Number(v1)
+// v2 = Number(v2)
 
-let resultado
+// let resultado
 
-switch(operacao){
-    case '+': resultado = v1+v2
-            break
-    case '-': resultado = v1-v2
-            break
-    case '*': resultado = v1*v2
-            break
-    case '/': resultado = v1/v2
-            break
-    default:
-        console.log("Operacao invalida")
-}
+// switch(operacao){
+//     case '+': resultado = v1+v2
+//             break
+//     case '-': resultado = v1-v2
+//             break
+//     case '*': resultado = v1*v2
+//             break
+//     case '/': resultado = v1/v2
+//             break
+//     default:
+//         console.log("Operacao invalida")
+// }
 
-console.log("Resultado de " + v1 + operacao + v2 + " = " + resultado)
+// console.log("Resultado de " + v1 + operacao + v2 + " = " + resultado)
+
+let valor1 = lerNumero('Digite o valor 1')
+let valor2 = lerNumero('Digite o valor 2')
+let op = leitor.question('Digite a operacao desejada (+, -, *, /)')
+let resultado = calculadora(op,valor1,valor2)
+console.log("Resultado de " + valor1 + op + valor2 + " = " + resultado)
--- funcoes.js
+++ funcoes.js
@@ -1,23 +1,64 @@
 import leitor from 'readline-sync'
 
-function lerNumero(){
-    let valor = leitor.question('Digite um numero: ')
-    valor = Number(valor) // tenta converter em um numero, se conseguir retorna um numero, se nao conseguir retorna NaN - not a number
-    while(valor=='' || Number.isNaN(valor)){
-        valor = leitor.question('Tente novamente: ')
-        valor = Number(valor)
+// function lerNumero(pergunta){
+//     let valor = leitor.question(pergunta)
+//     valor = Number(valor) // tenta converter em um numero, se conseguir retorna um numero, se nao conseguir retorna NaN - not a number
+//     while(valor=='' || Number.isNaN(valor)){
+//         valor = leitor.question('Tente novamente: ')
+//         valor = Number(valor)
+//     }
+
+//     return valor
+// }
+
+// // console.log('Ler valor 1: ')
+// // let v1 = lerNumero()
+// // console.log('Ler valor 2: ')
+// // let v2 = lerNumero()
+
+// // let soma = v1 + v2
+
+// // console.log('Soma de v1 e v2 eh: ' + soma)
+
+
+// function calculadora(operacao, v1, v2){
+//     let resultado
+//     switch (operacao) {
+//         case '+': resultado = v1 + v2
+//             break
+//         case '-': resultado = v1 - v2
+//             break
+//         case '*': resultado = v1 * v2
+//             break
+//         case '/': resultado = v1 / v2
+//             break
+//         default:
+//             console.log("Operacao invalida")
+//     }
+
+//     return resultado
+// }
+
+// console.log(calculadora('+',2,3))
+// console.log(calculadora('-',2,3))
+// console.log(calculadora('*',2,3))
+// console.log(calculadora('/',2,3))
+// console.log(calculadora(')',2,3))
+// calculadora('?',2,3)
+
+// export{lerNumero}
+// export{calculadora}
+
+
+function somaLista(lista){
+    let soma = 0
+
+    for(let i=0;i<lista.length;i++){
+        soma += lista[i]
     }
 
-    return valor
+    return soma
 }
 
-console.log('Ler valor 1: ')
-let v1 = lerNumero()
-console.log('Ler valor 2: ')
-let v2 = lerNumero()
-
-let soma = v1 + v2
-
-console.log('Soma de v1 e v2 eh: ' + soma)
-
-
+let l = [1,2,3,4,5]
+console.log(somaLista(l))
